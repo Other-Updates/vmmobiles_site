5737847959966a7502b4b241718f3d68517680ab
suryabui <suryabui2k22@gmail.com>
2022-06-11 12:49:29 +0530

    Files Add

---
+++ admin/language/en-gb/common/footer.php
@@ -0,0 +1,4 @@
+<?php
+// Text
+$_['text_footer']  = '<a target="_blank" href="https://f2fsolutions.co.in/">F2F Solutions</a> &copy; ' . date('Y') . ' All Rights Reserved.';
+$_['text_version'] = 'Version %s';
