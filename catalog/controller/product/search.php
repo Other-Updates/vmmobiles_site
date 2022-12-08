d4b728aef73236c12315558005a85154a05f40bd
ramkibui <ramkibui2k22@gmail.com>
2022-12-08 12:38:48 +0530

    Dev-V1.0.11.26-Show_Product_details_in_search

--- catalog/controller/product/search.php
+++ catalog/controller/product/search.php
@@ -240,6 +240,7 @@ class ControllerProductSearch extends Controller {
 					'qty'    	  => $result['quantity'],
 					'brand'        => $result['manufacturer'],
 					'review'        => $result['reviews'],
+					'attribute_groups'       => $this->model_catalog_product->getProductAttributes($result['product_id']),
 					'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 					'price'       => $price,
 					'special'     => $special,
